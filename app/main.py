2309b691194768a4f819fa11af60578c863e86b5
Pablo Seixas <dev.pseixas@gmail.com>
2024-04-11 16:57:38 -0300

    Inicializar novo repositório

---
+++ app/main.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+from fastapi import FastAPI
+from app.routes import router as api_router
+
+app = FastAPI()
+
+# Incluir rotas do arquivo routes.py
+app.include_router(api_router, prefix="/api")
+
+# Definir rota raiz
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, world!"}
